953a0031b9b8300de531a4cf6c31b8b43918f568
SimonJTremblay <stremblay@coach.ca>
2020-04-07 15:00:27 -0400

    added vuefont-aweome and buefy

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(Buefy, {
+  defaultIconPack: 'fa'
+})
 
 Vue.config.productionTip = false
 
