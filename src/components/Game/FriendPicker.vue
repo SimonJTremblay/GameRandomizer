620ccf13f8be4ac9e3ca8db673256555ddb8ece0
SimonJTremblay <stremblay@coach.ca>
2020-04-13 13:27:12 -0400

    Added FriendPicker + players and score working.

---
+++ src/components/Game/FriendPicker.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="block">
+        <b-field grouped >
+            <b-field>
+                <b-autocomplete placeholder="Player"
+                    icon-pack="fas"
+                    clearable
+                    icon="user"
+                    :data="filteredFriends"
+                    v-model="dataName"
+                    field="name"
+                    @select=" option => $emit('input', option)">
+                </b-autocomplete>
+            </b-field>
+        </b-field>
+    </section>
+</template>
+
+<script>
+export default {
+    props: ['value','friendList'],
+    data(){
+        return{
+            dataName:''
+        }
+    },
+    computed:{
+        filteredFriends(){
+            return this.friendList.filter ( option => {
+                return option.name
+                    .toString()
+                    .toLowerCase()
+                    .includes(this.dataName.toLowerCase())
+            })
+        }
+    }
+}
+</script>
--- src/components/Game/RecordGameModal.vue
+++ src/components/Game/RecordGameModal.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <form>
                 <b-field label="Select Game">
-                    <b-select placeholder="Select a game" v-model="selectedGame">
+                    <b-select placeholder="Select a game" v-model="selectedGame" expanded>
                         <option 
                             v-for="(game,index) in collection"
                             :key="index"
@@ -15,26 +15,57 @@
                 </b-field>
             </form>
             <div v-if="selectedGame">
-                <p>NbPlayers:  {{ selectedGame.players }}</p>
+                <label for="nbPlayers">Number of players:</label>
                 <div class="block">
                     <b-radio 
                         v-for="n in playersRange" 
                         :key="n"
                         v-model="selectedNumberOfPlayers"
-                        name="name"
+                        name="nbPlayers"
                         :native-value="n + (selectedGame.players.min - 1)"
                     >
                     {{ n + (selectedGame.players.min - 1) }}
                     </b-radio>
                 </div>
                 <p class="content"> #players:  {{ selectedNumberOfPlayers }}</p>
+                <div class="block">
+                    <b-field grouped v-for="n in selectedNumberOfPlayers" :key="n">
+                        <app-friend-picker v-model="selectedFriends[n-1]" :friendList="friends"> </app-friend-picker>
+                        <b-field expanded>
+                            <b-input 
+                                placeholder="Score"
+                                type="number"
+                                v-model.number="score[n-1]">
+                            </b-input>
+                        </b-field>
+                    </b-field>
+                    <p class="content">Players: {{selectedFriends}}</p>
+                    <p class="content">Score: {{score}}</p>
+                </div>
+                <b-field>
+                    <b-datepicker
+                        ref="datepicker"
+                        expanded
+                        editable
+                        placeholder="Select a date">
+                    </b-datepicker>
+                    <b-button
+                        @click="$refs.datepicker.toggle()"
+                        icon-left="calendar-day"
+                        type="is-primary" />
+                </b-field>
+                <b-button class="is-primary">Save</b-button>
             </div>
         </div>
     </section>
 </template>
 
 <script>
+import FriendPicker from './FriendPicker'
 export default {
+    components: {
+        'app-friend-picker': FriendPicker,
+    },
     props: {
         game:{
           type: Object,
@@ -75,6 +106,23 @@ export default {
                     complexity: '4/5'
                 }
             ],
+            friends:
+            [
+                {
+                    name:'Maxime',
+                    score: 0
+                },
+                {
+                    name:'Alexandra',
+                    score: 0
+                },
+                {
+                    name:'Jérémy',
+                    score: 0
+                },
+            ],
+            selectedFriends:[],
+            score:[],
             selectedGame: '',
             selectedNumberOfPlayers: -1
         };
@@ -82,11 +130,14 @@ export default {
     computed:{
         playersRange(){
             return this.selectedGame.players.max - this.selectedGame.players.min + 1;   // inclusive of max number
-        }
+        },
     },  // computed
     watch: {
         selectedGame(){
             this.selectedNumberOfPlayers = this.selectedGame.players.min;
+        },
+        selectedNumberOfPlayers(){
+            this.selectedFriends = this.selectedFriends.slice(0,this.selectedNumberOfPlayers)
         }
     }
 }
