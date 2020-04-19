af05e704439b25426d1be79581ccd1b3ea5f4d17
SimonJTremblay <stremblay@coach.ca>
2020-04-19 15:50:01 -0400

    Modified Search Bar to handle/display axios response

--- src/components/Search/SearchBar.vue
+++ src/components/Search/SearchBar.vue
@@ -14,25 +14,45 @@
                 <button class="button is-primary is-large" @click="searchForItem">Search</button>
             </p>
         </b-field>
+        <div v-if="searchResult.length">
+                <h4>{{ searchResult.length }} records</h4>
+            Selected game: {{ selectedGame.name._attributes.value }}
+            <b-table class="table"
+                :data="searchResult"
+                :columns="columns"
+                :selected.sync="selectedGame">
+            </b-table>
+        </div>
     </section>
 </template>
 
 <script>
+import bggApi from '../../axios/boardgamegeekFacade'
 export default {
-    data: function() {
+    data() {
         return{
-            itemToSearch: ''
+            itemToSearch: '',
+            searchResult:[],
+            selectedGame:'',
+            columns:[
+                {
+                    field: 'name._attributes.value',
+                    label:'Name'
+                },
+                {
+                    field: 'yearpublished._attributes.value',
+                    label:'Year Published',
+                    numeric: true
+                }
+            ]
         };
     },
     methods:{
-        searchForItem(){
-            alert(`${this.itemToSearch} was searched!`);
+        async searchForItem(){
+            let res = await bggApi.GetSearchItem(this.itemToSearch);
+            this.searchResult = res.items.item;
         }
     },
     
 }
 </script>
-
-<style scoped>
-
-</style>>
