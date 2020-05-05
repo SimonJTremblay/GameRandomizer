a5bc80070fddb62d74d6ad34de629aa9570dcb8a
SimonJTremblay <sjomp103@gmail.com>
2020-05-05 13:59:55 -0400

    Added constant variable to facilitate testing

--- src/axios/gameFacade.js
+++ src/axios/gameFacade.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { TEST_USER_ID } from '../constants'
 // let convert = require('xml-js');
 
 export const HTTP = axios.create({
@@ -18,6 +19,6 @@ export default{
           })
     },
     postGameToDatabase(game){
-        return this.execute('POST',`/${'5ea84cba73133255a8959863'}`, game)
+        return this.execute('POST',`/${TEST_USER_ID}`, game)
     }
 }
